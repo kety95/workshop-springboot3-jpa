9ed7321587758672f3bbdfde80cf24ff27dda967
Ketlyn <ketlyn.m@aluno.ifsp.edu.br>
2024-03-04 14:39:23 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/ketlyn/course/config/TestConfig.java
+++ src/main/java/com/ketlyn/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.ketlyn.course.entities.Category;
 import com.ketlyn.course.entities.Order;
+import com.ketlyn.course.entities.OrderItem;
 import com.ketlyn.course.entities.Product;
 import com.ketlyn.course.entities.User;
 import com.ketlyn.course.entities.enums.OrderStatus;
 import com.ketlyn.course.repositories.CategoryRepository;
+import com.ketlyn.course.repositories.OrderItemRepository;
 import com.ketlyn.course.repositories.OrderRepository;
 import com.ketlyn.course.repositories.ProductRepository;
 import com.ketlyn.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -69,5 +74,14 @@ public class TestConfig implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		
 	}
 }
